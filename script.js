ef05a19debf2e41afe95af9b8504117495ab8e25
lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-19 00:58:35 -0700

    modify code to work with buttons instead of prompts for one round

--- script.js
+++ script.js
@@ -1,106 +1,78 @@
+
+let humanScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
+const maxRounds = 5;
+let humanChoice = "";
+let computerChoice = "";
+
 function getComputerChoice (){
-    let number = Math.floor(Math.random() * 3); //randomly picks a whole number between 0-2  
-    console.log("---------------------------------------------------");
-    let result;
-    if (number === 0 ){ //if statement sets number to rock/paper/scissors
+    let number = Math.floor(Math.random() * 3); 
+    let result = "";
+    if (number === 0 ){
         result = "rock"
     } else if (number === 1) { 
         result = "paper"
     } else {
-        result = "scissors"
+        result = "scissor"
     }
     return result
 }
- //generates a number between 0-2, depending on the number output rock/paper/scissors
- 
-
-function getHumanChoice () {
-    let answer = prompt("Lets play rock, paper, scissors! What do you choose?");
-    answer = answer.toLowerCase();
-    return answer
-}   
-//prompts user to enter rock/paper/scissor, output user answer
 
 
-let humanScore = 0;
-let computerScore = 0;
+let button = Array.from(document.querySelectorAll("button"));
 
-function playRound (humanChoice, computerChoice) {
-    if (humanChoice === "paper" && computerChoice === "paper"){
-        return alert ("Its a tie. No one wins this time!")
-    } else if (humanChoice === "rock" && computerChoice === "rock"){
-        return alert ("Its a tie. No one wins this time!")
-    } else if (humanChoice === "scissors" && computerChoice === "scissors"){
-        return alert ("Its a tie. No one wins this time!")
-    } else if (humanChoice === "rock" && computerChoice === "scissors"){
-        humanScore = humanScore + 1;
-        return alert ("Rock beats Scissors. You won this round! 🎉");
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-        humanScore = humanScore + 1;
-        return alert ("Scissors beats Paper. You won this round! 🎉");
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        humanScore = humanScore + 1;
-        return alert ("Paper beats Rock. You won this round! 🎉");
-    } else if (humanChoice === "scissors" && computerChoice === "rock"){
-        computerScore = computerScore + 1;
-        return alert ("Rock beats Scissors. Sorry the computer wins 😞.");
-    } else if (humanChoice === "paper" && computerChoice === "scissors"){
-        computerScore = computerScore + 1;
-        return alert ("Scissors beats Paper. Sorry the computer wins 😞.");
-    } else if (humanChoice === "rock" && computerChoice === "paper"){
-        computerScore = computerScore + 1;
-        return alert("Paper beats Rock. Sorry the computer wins 😞.");
-    }
-}
-//comparison function to determine winner and increases score counter
-
-const body = document.querySelector("body")
-
-const div = document.createElement("div")
-const rockBtn = document.createElement("button")
-const paperBtn= document.createElement("button")
-const scissorBtn = document.createElement("button")
-
-
-div.appendChild(rockBtn);
-rockBtn.textContent = "Rock"
-div.appendChild(paperBtn);
-paperBtn.textContent = "Paper"
-div.appendChild(scissorBtn);
-scissorBtn.textContent = "Scissor"
-body.appendChild(div);
-
-const button = Array.from(document.querySelectorAll("button"));
-
-button.forEach((button) => {
-    button.addEventListener("click", () => {
-        playRound("rock", "paper");
+button.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        humanChoice = btn.innerText.toLowerCase();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     });
 });
 
 
+function playRound (humanChoice, computerChoice) {
+    let message = document.querySelector("#message")
+    let score = document.querySelector("#score")
 
-function playGame (){
-    for (let i=1; i<=5; i++){ //for loop to run the game 5 times
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
-        console.log(`You: ${humanSelection}  Computer: ${computerSelection}`)
-        playRound(humanSelection, computerSelection); //the function is only called here so it only runs 5 times.
-    } 
-    let winner;
-    if (humanScore > computerScore){  //compares scores to output a msg for the winner
-        console.log("---------------------------------------------------")
+    if (humanChoice === computerChoice){
+        message.innerText = `You chose ${humanChoice} and computer chose ${computerChoice}.`
+        score.innerText = `Its a tie. Current score: you ${humanScore} points, computer ${computerScore} points.`
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "scissor" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ){
+        humanScore++;
+        message.innerText = `You chose ${humanChoice} and computer chose ${computerChoice}.`
+        score.innerText = `You win! Current score: you ${humanScore} points, computer ${computerScore} points.`
+
+        
+    } else {
+        computerScore++;
+        message.innerText = `You chose ${humanChoice} and computer chose ${computerChoice}.`
+        score.innerText = `You lose! Current score: you ${humanScore} points, computer ${computerScore} points.`
+    }   
+}  
+    
+
+
+/*
+let winner = "";
+
+    if (humanScore > computerScore){ 
         winner = "WINNER WINNER CHICKEN DINNER 🐤🐓🐔 You Win!!!!!!"
     } else if (humanScore < computerScore) {
-        console.log("---------------------------------------------------")
-        winner = "The Computer Wins! Better luck next time. (╥﹏╥)"
+        winner = "The Computer Wins! Better luck next time."
     } else {
-        console.log("---------------------------------------------------")
-        winner = "Its a tie! No one wins this time ☹"
+        winner = "Its a tie! No one wins this time."
     }
-    return winner
-}
 
-console.log(playGame());
+    return alert(winner);
+*/
 
 
+
+
+
+     
