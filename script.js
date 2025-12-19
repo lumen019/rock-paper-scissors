9de5274619dfacfc3c3785a6bac44eedf5632340
lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-19 01:05:47 -0700

    still broken but update possible round counter to end game

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ button.forEach((btn) => {
 function playRound (humanChoice, computerChoice) {
     let message = document.querySelector("#message")
     let score = document.querySelector("#score")
-
+    roundsPlayed++;
     if (humanChoice === computerChoice){
         message.innerText = `You chose ${humanChoice} and computer chose ${computerChoice}.`
         score.innerText = `Its a tie. Current score: you ${humanScore} points, computer ${computerScore} points.`
@@ -53,24 +53,27 @@ function playRound (humanChoice, computerChoice) {
         message.innerText = `You chose ${humanChoice} and computer chose ${computerChoice}.`
         score.innerText = `You lose! Current score: you ${humanScore} points, computer ${computerScore} points.`
     }   
+
+    if (roundsPlayed > maxRounds){
+        let winner = "";
+        if (humanScore > computerScore){ 
+            winner = "WINNER WINNER CHICKEN DINNER 🐤🐓🐔 You Win!!!!!!"
+        } else if (humanScore < computerScore) {
+            winner = "The Computer Wins! Better luck next time."
+        } else {
+            winner = "Its a tie! No one wins this time."
+        }
+
+    return alert(winner);
+    }
 }  
     
 
 
-/*
-let winner = "";
 
-    if (humanScore > computerScore){ 
-        winner = "WINNER WINNER CHICKEN DINNER 🐤🐓🐔 You Win!!!!!!"
-    } else if (humanScore < computerScore) {
-        winner = "The Computer Wins! Better luck next time."
-    } else {
-        winner = "Its a tie! No one wins this time."
-    }
 
-    return alert(winner);
-*/
 
+    
 
 
 
